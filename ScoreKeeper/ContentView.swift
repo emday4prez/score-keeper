4d40ddf7e9d00267c5f17d07478dfa33b2a6cb01
Emerson <emerson.day@gmail.com>
2025-02-02 10:06:57 -0800

    first commit

--- ScoreKeeper/ContentView.swift
+++ ScoreKeeper/ContentView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var players : [String] = []
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
