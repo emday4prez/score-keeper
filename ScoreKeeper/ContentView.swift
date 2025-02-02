b3dfe6c42ae939e5bef9481c04e5b7b00ce3abc8
Emerson <emerson.day@gmail.com>
2025-02-02 11:24:01 -0800

    section 1

--- ScoreKeeper/ContentView.swift
+++ ScoreKeeper/ContentView.swift
@@ -8,14 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var players : [String] = []
+    @State private var players : [String] = ["Peter", "Paul", "Mary"]
     
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            ForEach(0..<players.count, id:\.description){ index in
+                TextField("Name", text: $players[index])
+            }
+            Button("Add Player", systemImage: "plus"){
+                players.append("")
+            }
         }
         .padding()
     }
